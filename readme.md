e8ebf47673bfff298e94722bd5956c4f5e98c379
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2021-08-05 13:40:23 +0530

    asset managment

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Getting Started
+
+## Asset Management in Webpack
+
+- In order to import a CSS file from within a JavaScript module, you need to install and add the style-loader and css-loader to your module configuration
+- In order to bundle media assets like png/jpeg/gif/video/font etc. we can use built-in assets module
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from "lodash";
+import "./style.css";
+import myImage from "./BlogDesign.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
+  element.classList.add("hello");
+  const image = new Image();
+  image.src = myImage;
+  element.appendChild(image);
   return element;
 }
 
